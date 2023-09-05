3d5b43672d1bf8c2f80ac9d0aa383302adf948e2
Wandy-Parzy <wandypaulino05@gmail.com>
2023-09-04 22:17:31 -0400

    Agregando diseño

--- app/src/main/java/com/example/dialogocompose/MainActivity.kt
+++ app/src/main/java/com/example/dialogocompose/MainActivity.kt
@@ -4,38 +4,54 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dialogocompose.ui.theme.DialogoComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DialogoComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     var show by rememberSaveable { mutableStateOf(false) }
 
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        Button(onClick = { show = true }) {
-                            Text(text = "Dialogo")
-
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(
+                            onClick = { show = true },
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(text = "Mostrar Diálogo")
+                        }
+                        MyDialog(show, { show = false }) {
                         }
                     }
-                    MyDialog(show, { show = false }, {Log.i("wandy", "clik")})
-
                 }
             }
         }
@@ -49,20 +65,49 @@ fun MyDialog(
     onConfirm: () -> Unit
 ) {
     if (show) {
-
-        AlertDialog(onDismissRequest = { /*TODO*/ },
+        AlertDialog(
+            onDismissRequest = { onDismiss() },
+            title = {
+                Text(
+                    text = "Diálogo",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            },
+            text = {
+                Text(
+                    text = "¡Bienvenido! Esto es un diálogo",
+                    color = Color.Gray,
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(8.dp)
+                )
+            },
             confirmButton = {
-                TextButton(onClick = { onConfirm() }) {
+                Button(
+                    onClick = {
+                        onConfirm()
+                        onDismiss()
+                    },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
+                ) {
                     Text(text = "Confirmar")
                 }
             },
             dismissButton = {
-                TextButton(onClick = { onDismiss() }) {
+                Button(
+                    onClick = { onDismiss() },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                ) {
                     Text(text = "Cancelar")
                 }
             },
-            title = { Text(text = "Dialogo") },
-            text = { Text(text = "Bienvenido, esto es un dialogo") }
+            modifier = Modifier.width(300.dp)
         )
     }
 }
@@ -71,7 +116,6 @@ fun MyDialog(
 @Composable
 fun DefaultPreview() {
     DialogoComposeTheme {
-
 
     }
 }
